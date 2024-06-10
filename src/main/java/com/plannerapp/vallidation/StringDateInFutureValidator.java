2c0b0c3fa818a3a6500749f2108dd39ad9d2ecdf
Sonya Karafeizova <sonqkarafeizova@gmail.com>
2024-06-10 18:16:50 +0300

    Upload

---
+++ src/main/java/com/plannerapp/vallidation/StringDateInFutureValidator.java
@@ -0,0 +1,26 @@
+package com.plannerapp.vallidation;
+
+import com.plannerapp.model.annotation.StringDateInFuture;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.lang.annotation.Annotation;
+import java.time.LocalDate;
+
+public class StringDateInFutureValidator implements ConstraintValidator<StringDateInFuture,String> {
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+        if (value != null && !value.isBlank()) {
+            LocalDate parse = LocalDate.parse(value);
+            return parse.isAfter(LocalDate.now());
+        }
+
+        return false;
+    }
+
+    @Override
+    public void initialize(StringDateInFuture constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+}
